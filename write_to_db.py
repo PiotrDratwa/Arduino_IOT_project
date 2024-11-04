e54e1dd3a94981225c70c076b9ac1e03e7f392ea
Piotr Dratwa <pdratwa@edu.cdv.pl>
2024-11-04 17:32:27 +0100

    added tls protocol

--- write_to_db.py
+++ write_to_db.py
@@ -1,4 +1,5 @@
-import paho.mqtt.client as mqtt
+from paho.mqtt import client as mqtt_client
+from random import randint
 
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
@@ -7,21 +8,12 @@ def on_connect(client, userdata, flags, reason_code, properties):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
-def on_subscribe(client, userdata, mid, reason_code_list, properties):
-    # Since we subscribed only for a single channel, reason_code_list contains
-    # a single entry
-    if reason_code_list[0].is_failure:
-        print(f"Broker rejected you subscription: {reason_code_list[0]}")
-    else:
-        print(f"Broker granted the following QoS: {reason_code_list[0].value}")
+client_id = f'python-mqtt-{randint(0, 1000)}'
+client = mqtt_client.Client(client_id)
+client.tls_set(ca_certs='./broker.emqx.io-ca.crt')
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("broker.emqx.io", 8883)
 
-
-
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
-
-mqttc.connect("broker.emqx.io", 8883)
-
-mqttc.loop_forever()
-print(f"Received the following message: {mqttc.user_data_get()}")
+client.loop_forever()
+print(f"Received the following message: {client.user_data_get()}")
